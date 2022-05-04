e6d1ad4b19a3766e25120513bd482d46e3aa4574
Areccus Branch <areccusbranch@Areccuss-MacBook.local>
2022-05-04 01:46:57 -0400

    new commit

--- src/Components/TopBar/Logo/Logo.js
+++ src/Components/TopBar/Logo/Logo.js
@@ -3,7 +3,7 @@ import react from "react";
 const Logo = () => {
     return(
         <div className="ma mt0">
-            <h3 className="pa3 b i georgia">S.T.A.R</h3>
+            <a href="#home"><h3 className="pa3 b i georgia">S.T.A.R</h3></a>
         </div>
     )
 }
